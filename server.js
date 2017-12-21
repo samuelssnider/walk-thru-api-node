08f0ea63113723f8980156a2eeb89d5dd16baf3e
samuelssnider <sam_redins@yahoo.com>
2017-12-21 15:09:13 -0700

    added knex and pg

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const environment = process.env.NODE_ENV || 'development';    // if something else isn't setting ENV, use development
+const configuration = require('./knexfile')[environment];    // require environment's settings from knexfile
+const database = require('knex')(configuration); 
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
+
 
 app.locals.title = "Walk Through API"
 app.set('port', process.env.PORT || 3000)
@@ -7,6 +12,7 @@ app.set('port', process.env.PORT || 3000)
 app.get('/', function(request, response) {
   response.send("Replace with API docs")
 })
+
 
 if (!module.parent) {
   app.listen(app.get('port'), function() {
--- test/server-test.js
+++ test/server-test.js
@@ -2,8 +2,22 @@ var assert = require('chai').assert;
 var app = require('../server');
 
 describe('Server', function() {
+  
+  before(function(done) {
+    this.port = 9876;
+    this.server = app.listen(this.port, function(err, result) {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(function() {
+    this.server.close();
+  });
+  
   it('should exist', function() {
     assert(app);
   })
   
+    
 })
