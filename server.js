711a6ec874f6ad0fe01d37260a1cc4dde055c20d
samuelssnider <sam_redins@yahoo.com>
2017-12-21 14:30:54 -0700

    title added, as well as message with server port

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.locals.title = "Walk Through API"
 
 app.get('/', function(request, response) {
   response.send("Replace with API docs")
+})
+
+app.listen(3000, function(){
+  console.log(`${app.locals.title} is running on port 3000`)
 })
