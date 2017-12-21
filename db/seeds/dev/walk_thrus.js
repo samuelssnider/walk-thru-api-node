c7000d09f42fe9dd4ce554c942221dafb19e163a
samuelssnider <sam_redins@yahoo.com>
2017-12-21 16:14:14 -0700

    seed for walk_thrus created

--- db/seeds/dev/walk_thrus.js
+++ db/seeds/dev/walk_thrus.js
@@ -1,21 +1,24 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('TRUNCATE walk_thrus RESTART IDENTITY')
+  return knex.raw('TRUNCATE walk_thrus RESTART IDENTITY CASCADE')
     .then(function () {
       // Inserts seed entries
       return knex('walk_thrus').insert([
         {
          title: 'Go Skiing', 
-         description: 'Go up to the mountains and slide down a snowy hill!'
+         description: 'Go up to the mountains and slide down a snowy hill!',
+         created_at: new Date
         },
         {
           title: 'Bake Cookies', 
-          description: 'Mix em, Bake em, Eat em'
+          description: 'Mix em, Bake em, Eat em',
+          created_at: new Date
         },
         { 
           title: 'Beat Super Mario Level One', 
-          description: "It's so hard though"
+          description: "It's so hard though",
+          created_at: new Date
         }
       ]);
     });
