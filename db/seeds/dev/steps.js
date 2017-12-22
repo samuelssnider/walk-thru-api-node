3f99f1e602451d5ccb4a977af6b98ad60ea4aa32
samuelssnider <sam_redins@yahoo.com>
2017-12-22 12:36:23 -0700

    seeds work in hacky way, trying to solve

---
+++ db/seeds/dev/steps.js
@@ -0,0 +1,45 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex.raw('TRUNCATE steps RESTART IDENTITY CASCADE')
+  .then(function () {
+    // Inserts seed entries
+    return knex('steps').insert([
+      {
+        walk_thruid: 1,
+        title: 'Get Equipment', 
+        description: 'Go buy it!',
+        created_at: new Date
+      },
+      {
+        walk_thruid: 1,
+        title: 'Buy a pass', 
+        description: 'Go buy that too!',
+        created_at: new Date
+      },
+      {
+        walk_thruid: 1,
+        title: 'Hit the slopes', 
+        description: "You know you want to!",
+        created_at: new Date
+      },
+      {
+        walk_thruid: 2,
+        title: 'Pour and mix it', 
+        description: 'Go mix it!',
+        created_at: new Date
+      },
+      {
+        walk_thruid: 2,
+        title: 'Break and bake', 
+        description: 'Go bake it!',
+        created_at: new Date
+      },
+      {
+        walk_thruid: 2,
+        title: 'Stir it Up', 
+        description: "You know you want to!",
+        created_at: new Date
+      }
+    ]);
+  });
+};
