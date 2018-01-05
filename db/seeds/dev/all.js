670a8c7c27c53ef0c2e237717336b0e6d51d8631
samuelssnider <sam_redins@yahoo.com>
2018-01-05 10:13:45 -0700

    add users seed and migrate

--- db/seeds/dev/all.js
+++ db/seeds/dev/all.js
@@ -1,6 +1,7 @@
 const walkThrus    = require('../data/walk_thrus')
 const steps        = require('../data/steps')
 const instructions = require('../data/instructions')
+const users        = require('../data/users')
 
 exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE instructions RESTART IDENTITY CASCADE')
@@ -11,6 +12,9 @@ exports.seed = function(knex, Promise) {
     return knex.raw('TRUNCATE walk_thrus RESTART IDENTITY CASCADE')
   })
   .then(function () {
+    return knex.raw('TRUNCATE users RESTART IDENTITY CASCADE')
+  })
+  .then(function () {
     return knex('walk_thrus').insert(walkThrus['walkThrus']);
   })
   .then(function () {
@@ -18,5 +22,8 @@ exports.seed = function(knex, Promise) {
   })
   .then(function () {
     return knex('instructions').insert(instructions['instructions']);
+  })
+  .then(function () {
+    return knex('users').insert(users['instructions']);
   })
 };
