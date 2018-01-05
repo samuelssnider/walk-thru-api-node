e3eba52c740d5867e5a1a2239b100512128c7edb
samuelssnider <sam_redins@yahoo.com>
2018-01-05 10:14:54 -0700

    express session, session file store, and morgan

---
+++ db/migrations/20180105093308_create_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  let createQuery = `CREATE TABLE users(
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR (50),
+    password TEXT,
+    created_at TIMESTAMP
+  )`
+  return knex.raw(createQuery)
+}
+
+exports.down = function(knex, Promise) {
+  let dropQuery = `DROP TABLE users`
+  return knex.raw(dropQuery)
+}
---
+++ db/seeds/data/users.js
@@ -0,0 +1,20 @@
+var faker = require('faker')
+const users = [{
+  username: "sam",
+  password: "sam",
+  created_at: new Date
+}]
+
+function createRandomUser(i) {
+  return randomUser = {
+    name: faker.internet.userName(i),
+    password: faker.internet.userName(i),
+    created_at: new Date
+  }
+}
+
+for(var i = 0; i < 10; i ++){
+  users.push(createRandomUser(i))
+}
+
+module.exports = { users }
