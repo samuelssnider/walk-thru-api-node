68bbbee4f237862da358738c2f6d79146d9b7897
samuelssnider <sam_redins@yahoo.com>
2018-01-15 11:59:13 -0700

    working off master, heroku keys

--- knexfile.js
+++ knexfile.js
@@ -27,8 +27,8 @@ module.exports = {
     client: 'pg',
     connection: {
       database: 'postgres://kxqvuxeukkbpvp:5c92997d95e3a058d6cbfceb00fd17aefcab836e73f3c8f3d7df721e75ae1937@ec2-23-21-246-25.compute-1.amazonaws.com:5432/dbfdaqn2tdpngl',
-      user:     'username',
-      password: 'password'
+      user:     'sam',
+      password: 'sam'
     },
     migrations: {
       directory: './db/migrations'
