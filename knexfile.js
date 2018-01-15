0623c1c178b6892f0a81b68c95cfa4d2135b15b3
samuelssnider <sam_redins@yahoo.com>
2018-01-15 12:30:05 -0700

    added new database

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-            database: 'walk_thrus_prod',
+            database: 'postgres://damnbkjxpwkmdy:4c03254f7413cfcfda6876bd79043f66fbf7a68c57bba1aec08abe61d655f2f2@ec2-54-227-250-33.compute-1.amazonaws.com:5432/df7fk7hlbsb26r',
         },
     debug: true,
     migrations: {
