ee0b76d22545d9bfb2665dc77bfd4a8586a0e030
samuelssnider <sam_redins@yahoo.com>
2018-01-16 20:51:15 -0700

    update seeds to uncommented

--- knexfile.js
+++ knexfile.js
@@ -30,9 +30,9 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     },
-    // seeds: {
-    //   directory: './db/seeds/dev'
-    // },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
   }
 }
